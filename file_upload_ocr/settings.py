c6fa5f3e9f2c2792db4944876450fe9a3b3499e5
sanoj2k <sanojperror2k@gmail.com>
2025-02-17 12:42:10 +0530

    debug false

--- file_upload_ocr/settings.py
+++ file_upload_ocr/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-etchd7klaj!mot9&-$_3d=pe40#@^4_x-+k$zrh(5i%-^ah26^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['43.204.130.41', '127.0.0.1']
 
 
 # Application definition
